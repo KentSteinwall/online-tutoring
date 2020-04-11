ef6ac8a661f6de4e54296c0cc2ba4b2e2b434c29
KentSteinwall <kentsteinwall@gmail.com>
2020-04-11 19:30:47 +0530

    add student dashboard

--- client/Project/src/app/app-routing.module.ts
+++ client/Project/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { TutorsComponent } from './student/tutors/tutors.component';
 
 
 const routes: Routes = [
 {  path:"home" ,component:HomeComponent},
 {  path:"signin" ,component:SigninComponent},
 {  path:"signup" ,component:SignupComponent},
+{  path:"tutors" ,component:TutorsComponent},
 
 
 ];
